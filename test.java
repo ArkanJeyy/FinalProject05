ddc1c23e9357aab6aedfdd94a043dce7f3172a50
ArkanJeyy <arkanfarrasabyan@gmail.com>
2025-09-25 09:15:35 +0700

    aku  push "yok lah main"

--- test.java
+++ test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama");
         System.out.println("halo");
+        System.out.println("Yok lah main");
     }
 }
